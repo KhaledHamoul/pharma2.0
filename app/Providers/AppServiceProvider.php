d2a49684e034eba3a2c211ff5a62bf71135f2428
khaledHamoul <fk_hamoul@esi.dz>
2019-08-15 18:15:14 +0100

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider {
+    public function register() {
+        
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot() {
+	    Schema::defaultStringLength(191);
+    }
+}
